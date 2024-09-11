7d09264867460a55957880f8ef0480ac76800480
chlkstn <chalkstoneinteractive@gmail.com>
2024-09-11 14:35:13 +0200

    adding the searchcomponent and updating the SearchPage

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,16 @@
+import Search from "../components/Search";
+
+import { SearchContextProvider } from "../contexts/SearchResultContext";
+
 export function SearchPage() {
   return (
-    <div>SearchPage</div>
-  )
+    <>
+      <div>Search Page</div>
+      <div>
+        <SearchContextProvider>
+          <Search />
+        </SearchContextProvider>
+      </div>
+    </>
+  );
 }
