8ae2a84ef7b0b004fcb28338b41d83c3b71d8a54
ninokhodr <rudie.khodr@gmail.com>
2024-09-11 14:34:07 +0200

    Created A Randomize Button, that navigates back to home by a onClick event, also updated CSS for better styling

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
-import { HomePage, SearchPage,NotFound } from "./pages";
+import { HomePage, SearchPage, NotFound } from "./pages";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />} path="/">
       <Route element={<HomePage />} index />
       <Route element={<SearchPage />} path="search" />
-            <Route  element={<NotFound />} path="*" />
-
+      <Route element={<NotFound />} path="*" />
     </Route>
-));
+  )
+);
