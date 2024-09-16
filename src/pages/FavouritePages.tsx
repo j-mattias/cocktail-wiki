684790cf03630b2fd14edacc9538816bff5552e9
vw rabbit <vkutov@gmail.com>
2024-09-16 15:25:02 +0200

    the favourites needs to be implemented

---
+++ src/pages/FavouritePages.tsx
@@ -0,0 +1,14 @@
+import { useLoaderData, useNavigate } from "react-router-dom";
+import { createContext, ReactElement, ReactNode, useState } from "react";
+
+import { CocktailCard } from "../components";
+import { IDrinkReformat } from "../interfaces";
+  interface IMovieProviderProps {
+  children: ReactNode;
+}
+
+export function FavouritePages() {
+
+// hooks come here  
+}
+
