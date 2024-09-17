4b0114f39415639ff1012c2473eb5aa76ed940ca
ninokhodr <rudie.khodr@gmail.com>
2024-09-17 14:28:11 +0200

    useEffect hook implemented - extracting id:s from correct url params , cocktail info page now showing right details

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import App from "./App";
-import { HomePage, SearchPage, NotFound, FavouritePage } from "./pages";
+import { HomePage, SearchPage, NotFound, FavouritePage, CocktailDetailsPage } from "./pages"; 
 import { randomCocktailLoader } from "./loaders";
+
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -9,6 +10,7 @@ export const router = createBrowserRouter(
       <Route element={<HomePage />} loader={randomCocktailLoader} index />
       <Route element={<SearchPage />} path="search" />
       <Route element={<FavouritePage />} path="favourite" /> 
+      <Route element={<CocktailDetailsPage />} path="details/:id" />
       <Route element={<NotFound />} path="*" />
     </Route>
   )
