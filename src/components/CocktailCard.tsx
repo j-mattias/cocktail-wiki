1c14a0254db290535ff1086a470efda605e49dec
chlkstn <chalkstoneinteractive@gmail.com>
2024-09-24 11:06:41 +0200

    adding css for cards

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
 import { Cocktail } from "../interfaces";
+import "../css/CocktailCard.css";
 
 interface ICocktailCardProps {
   cocktail: Cocktail;
 }
 
-export const CocktailCard = ({ cocktail }: ICocktailCardProps): React.ReactElement => {
+export const CocktailCard = ({
+  cocktail,
+}: ICocktailCardProps): React.ReactElement => {
   return (
     <article className="cocktail-card">
       <figure className="cocktail-image">
-        <img src={cocktail.image} alt={cocktail.name} className="cocktail-image" />
+        <img
+          src={cocktail.image}
+          alt={cocktail.name}
+          className="cocktail-image"
+        />
       </figure>
+      {/* Placeholder for favorite button*/}
+      <div className="cocktail-fav">★</div>
+
       <h3 className="cocktail-info">{cocktail.name}</h3>
+
       <p>
         <Link to={`/details/${cocktail.id}`}>More info</Link>
       </p>
